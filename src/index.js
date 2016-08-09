9e8c6f675ead8f8c05005995b0a68f2e1bf65fe6
Kiran Kumar <kirankumar.86@gmail.com>
2016-08-09 14:00:09 -0700

    upload to gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MovieContainer from './App';
-import './index.css';
-
-ReactDOM.render(
-  <MovieContainer />,
-  document.getElementById('root')
-);
